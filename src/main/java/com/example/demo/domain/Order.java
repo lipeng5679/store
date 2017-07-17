4e9026c56c5d5bc5a7b3361a9f9eee9ac3cbb166
lipeng <645614418@qq.com>
2017-07-17 10:24:47 +0800

    后台

--- src/main/java/com/example/demo/domain/Order.java
+++ src/main/java/com/example/demo/domain/Order.java
@@ -23,11 +23,12 @@ public class Order implements Serializable{
     private Date submitTime;    //订单提交时间
     private Date conTime;       //预定时间
     private Double totalPrice;  //总金额
-    private int isPayoff;       //是否付款  0未付款  1已付款  2已退款
-    private int iscon;          //是否发货  0未发货  1已发货
-                                //是否评价  0未评价  1已评价
+    private int isPayoff;       //是否付款  0未支付  1已支付  2配送中  3已送达
+    private int iscon;          //是否评价  0未评价  1已评价 2已退款
+
 
     private User user;          //订单用户信息
     private List<Commodity> commodities;    //多个商品信息
-    private List<OrderList> orderLists;    //订单列表信息
+    private List<OrderList> orderLists;    //订单与商品之间的关系
+    private Module module;      //模块信息
 }
